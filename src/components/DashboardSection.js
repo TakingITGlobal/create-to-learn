d10570ec249a8a43ccca08d925ecda47513edbdc
Dina B <buricd6777@gmail.com>
2023-03-22 01:15:50 -0700

    Adds carousel component

--- src/components/DashboardSection.js
+++ src/components/DashboardSection.js
@@ -7,6 +7,11 @@ import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import Typography from '@material-ui/core/Typography'
 import LinkMui from '@material-ui/core/Link'
+import Carousel from 'react-material-ui-carousel'
+import MultiCarousel from 'react-multi-carousel'
+import 'react-multi-carousel/lib/styles.css'
+
+import { Paper, Button } from '@mui/material'
 import { makeStyles } from '@material-ui/core/styles'
 import Section from './Section'
 import SectionHeader from './SectionHeader'
@@ -18,7 +23,21 @@ const useStyles = makeStyles((theme) => ({
   cardContent: {
     padding: theme.spacing(3),
   },
+  carouselItem: {
+    padding: '10px',
+  },
 }))
+
+function Item(props) {
+  return (
+    <Paper>
+      <h2>{props.item.name}</h2>
+      <p>{props.item.description}</p>
+
+      <Button className="CheckButton">Check it out!</Button>
+    </Paper>
+  )
+}
 
 function DashboardSection(props) {
   const classes = useStyles()
@@ -41,7 +60,7 @@ function DashboardSection(props) {
           textAlign="center"
         />
 
-        {router.query.paid && auth.user.planIsActive && (
+        {/* {router.query.paid && auth.user.planIsActive && (
           <Box mx="auto" mb={4} maxWidth={400}>
             <Alert severity="success">
               You are now subscribed to the {auth.user.planId} plan
@@ -54,9 +73,9 @@ function DashboardSection(props) {
               </span>
             </Alert>
           </Box>
-        )}
+        )} */}
 
-        <Grid container={true} spacing={4}>
+        {/* <Grid container={true} spacing={4}>
           <Grid item={true} xs={12} md={6}>
             <DashboardItems />
           </Grid>
@@ -135,10 +154,130 @@ function DashboardSection(props) {
               </CardContent>
             </Card>
           </Grid>
-        </Grid>
+        </Grid> */}
+
+        <LearningPath />
+        <TopCourses />
       </Container>
     </Section>
   )
 }
 
 export default DashboardSection
+
+function LearningPath() {
+  var items = [
+    {
+      name: 'Photography Filming and Acting',
+      description:
+        'Throughout this unit, student will learn the basics of photography and filming',
+      time: '5 lessons, 1 hr 30 minutes',
+    },
+    {
+      name: 'Web development and design',
+      description:
+        'Throughout this unit, student will learn the basics of web development',
+      time: '7 lessons, 2 hr 30 minutes',
+    },
+  ]
+  return (
+    <>
+      <Box>
+        <Typography>Learning paths for students</Typography>
+      </Box>
+      <Carousel animation="slide" interval={null} swipe>
+        {items.map((item, i) => (
+          <Paper key={i} sx={{ padding: 2.5 }}>
+            <h2>{item.name}</h2>
+            <p>{item.description}</p>
+            <p>{item.time}</p>
+
+            <Box>
+              <Button color="primary" fullWidth variant="contained">
+                See details
+              </Button>
+            </Box>
+          </Paper>
+        ))}
+      </Carousel>
+    </>
+  )
+}
+
+function TopCourses() {
+  const classes = useStyles()
+
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+      paritialVisibilityGutter: 60,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+      paritialVisibilityGutter: 50,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+      paritialVisibilityGutter: 40,
+    },
+  }
+  var items = [
+    {
+      name: 'Photography Filming and Acting',
+      description:
+        'Throughout this unit, student will learn the basics of photography and filming',
+      time: '5 lessons, 1 hr 30 minutes',
+    },
+    {
+      name: 'Web development; coding and design',
+      description:
+        'Throughout this unit, student will learn the basics of web development',
+      time: '7 lessons, 2 hr 30 minutes',
+    },
+    {
+      name: 'Photography Filming and Acting',
+      description:
+        'Throughout this unit, student will learn the basics of photography and filming',
+      time: '5 lessons, 1 hr 30 minutes',
+    },
+    {
+      name: 'Web development; coding and design',
+      description:
+        'Throughout this unit, student will learn the basics of web development',
+      time: '7 lessons, 2 hr 30 minutes',
+    },
+  ]
+  return (
+    <>
+      <Box>
+        <Typography>Top Courses in Video & Film</Typography>
+      </Box>
+      <MultiCarousel
+        ssr
+        partialVisbile
+        responsive={responsive}
+        swipeable
+        itemClass={classes.carouselItem}
+      >
+        {items.map((item, i) => {
+          return (
+            <Paper key={i} sx={{ padding: 2.5 }}>
+              <h2>{item.name}</h2>
+              <p>{item.description}</p>
+              <p>{item.time}</p>
+
+              <Box>
+                <Button color="primary" fullWidth variant="contained">
+                  See details
+                </Button>
+              </Box>
+            </Paper>
+          )
+        })}
+      </MultiCarousel>
+    </>
+  )
+}
