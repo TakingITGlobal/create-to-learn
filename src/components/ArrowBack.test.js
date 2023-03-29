d2e334a3d1f54c51c8f14e26d291c5fd7836b5d1
Stephen Belyea <belyea.stephen@gmail.com>
2023-03-28 21:20:12 -0400

    Add axe a11y test for ArrowBack and drop context comments

--- src/components/ArrowBack.test.js
+++ src/components/ArrowBack.test.js
@@ -2,21 +2,28 @@ import * as React from 'react'
 import { MemoryRouter } from 'react-router-dom'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
+import { axe } from 'jest-axe'
 import ArrowBack from './ArrowBack'
 
 describe('ArrowBack component', () => {
+  it('passes basic a11y testing', async () => {
+    // Render the component within a mocked page body
+    render(<ArrowBack />, document.body)
+
+    // Use axe-core to test the full page body, including ArrowBack
+    const results = await axe(document.body)
+    expect(results).toHaveNoViolations()
+  })
+
   it('renders button version by default', () => {
-    render(
-      <MemoryRouter initialEntries={['/']}>
-        <ArrowBack />
-      </MemoryRouter>,
-    )
+    render(<ArrowBack />)
 
     expect(screen.getByRole('button').nodeName).toEqual('BUTTON')
     expect(screen.getByLabelText(/back to previous page/i)).toBeInTheDocument()
   })
 
   it('renders link to settings page when showComponent matches nav', () => {
+    // Since we're rendering a router Link, we need to mock the router context
     render(
       <MemoryRouter initialEntries={['/']}>
         <ArrowBack showComponent="nav" />
@@ -30,15 +37,10 @@ describe('ArrowBack component', () => {
   it('fires setShowComponent when button version is clicked', () => {
     const mockSetShowComponent = jest.fn()
     render(
-      <MemoryRouter initialEntries={['/']}>
-        <ArrowBack
-          showComponent="foo"
-          setShowComponent={mockSetShowComponent}
-        />
-      </MemoryRouter>,
+      <ArrowBack showComponent="foo" setShowComponent={mockSetShowComponent} />,
     )
 
     userEvent.click(screen.getByRole('button'))
-    expect(mockSetShowComponent).toHaveBeenCalled()
+    expect(mockSetShowComponent).toHaveBeenCalledTimes(1)
   })
 })
