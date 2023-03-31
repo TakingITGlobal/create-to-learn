7440c94883e6054fe1fcd5cacb6080b24bb0a276
Stephen Belyea <belyea.stephen@gmail.com>
2023-03-30 20:49:26 -0400

    Add mocking and test case for empty query.next

--- src/hooks/use-auth-form.test.js
+++ src/hooks/use-auth-form.test.js
@@ -1,27 +1,30 @@
 import { act } from '@testing-library/react'
 import { renderHook } from '@testing-library/react-hooks'
+import { useRouter } from '../util/router'
 import { useAuthForm } from './use-auth-form.hook'
 
 const mockPush = jest.fn()
+const mockQuery = {
+  type: 'foo',
+  next: '/bar',
+}
+const mockUseRouterReturn = {
+  push: mockPush,
+  query: mockQuery,
+}
 
-jest.mock('../util/router', () => ({
-  useRouter: () => ({
-    push: mockPush,
-    query: {
-      type: 'foo',
-      next: '/foobar',
-    },
-  }),
-}))
+jest.mock('../util/router')
 
 describe('useAuthForm hook', () => {
   it('returns type matching useRouter query.type', () => {
+    useRouter.mockImplementation(() => mockUseRouterReturn)
     const { result } = renderHook(() => useAuthForm())
 
-    expect(result.current.type).toBe('foo')
+    expect(result.current.type).toBe(mockQuery.type)
   })
 
   it('returns null formAlert by default', () => {
+    useRouter.mockImplementation(() => mockUseRouterReturn)
     const { result } = renderHook(() => useAuthForm())
 
     expect(result.current.formAlert).toBeNull()
@@ -29,6 +32,8 @@ describe('useAuthForm hook', () => {
 
   it('returns handleFormAlert which updates formAlert using provided data', () => {
     const mockFormAlert = { type: 'foo', message: 'bar' }
+
+    useRouter.mockImplementation(() => mockUseRouterReturn)
     const { result } = renderHook(() => useAuthForm())
 
     act(() => result.current.handleFormAlert(mockFormAlert))
@@ -38,10 +43,23 @@ describe('useAuthForm hook', () => {
   })
 
   it('returns handleAuth which calls router push with useRouter query.next', () => {
+    useRouter.mockImplementation(() => mockUseRouterReturn)
     const { result } = renderHook(() => useAuthForm())
 
     act(() => result.current.handleAuth())
 
-    expect(mockPush).toHaveBeenCalledWith('/foobar')
+    expect(mockPush).toHaveBeenCalledWith(mockQuery.next)
+  })
+
+  it('returns dashboard route for handleAuth if query.next is not found', () => {
+    useRouter.mockImplementation(() => ({
+      ...mockUseRouterReturn,
+      query: { ...mockQuery, next: null },
+    }))
+    const { result } = renderHook(() => useAuthForm())
+
+    act(() => result.current.handleAuth())
+
+    expect(mockPush).toHaveBeenCalledWith('/dashboard')
   })
 })
