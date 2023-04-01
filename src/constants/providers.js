f4531b8a704ededc032107725f64f04e3f034394
Stephen Belyea <belyea.stephen@gmail.com>
2023-03-31 20:49:11 -0400

    Add constants files for social providers, route types, and routing paths

---
+++ src/constants/providers.js
@@ -0,0 +1,4 @@
+export const PROVIDER_GOOGLE = 'google'
+export const PROVIDER_FACEBOOK = 'facebook'
+
+export const PROVIDERS = [PROVIDER_GOOGLE, PROVIDER_FACEBOOK]
---
+++ src/constants/route-types.js
@@ -0,0 +1,10 @@
+export const ROUTE_TYPE_SIGNUP = 'signup'
+export const ROUTE_TYPE_SIGNIN = 'signin'
+
+export const ROUTE_TYPES = {
+  SIGNUP: ROUTE_TYPE_SIGNUP,
+  SIGNIN: ROUTE_TYPE_SIGNIN,
+  FORGOT: 'forgotpass',
+}
+
+export const AUTH_ROUTE_TYPES = [ROUTE_TYPE_SIGNUP, ROUTE_TYPE_SIGNIN]
