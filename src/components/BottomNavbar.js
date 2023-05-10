39ef38a5437ba5a55e32f0524b714389f415a878
Dina B <buricd6777@gmail.com>
2023-05-09 22:45:10 -0700

    Removes navbar on sign-up page

--- src/components/BottomNavbar.js
+++ src/components/BottomNavbar.js
@@ -1,62 +1,64 @@
 import React from 'react'
 import BottomNavigation from '@mui/material/BottomNavigation'
 import BottomNavigationAction from '@mui/material/BottomNavigationAction'
-import Paper from '@mui/material/Paper';
+import Paper from '@mui/material/Paper'
 import HouseIcon from '@mui/icons-material/House'
 import PublicIcon from '@mui/icons-material/Public'
 import LibraryIcon from '@mui/icons-material/LocalLibrary'
 import AccountIcon from '@mui/icons-material/AccountCircle'
-import { Link } from './../util/router'
+import { Link, useLocation } from './../util/router'
 
 export default function BottomNavbar() {
-
   const [value, setValue] = React.useState(0)
-  
+  const { pathname } = useLocation()
+
   return (
-    <Paper 
-      variant="elavation"
-      elavation={0}
-      sx={{ 
-        position: 'fixed', 
-        bottom: 0, 
-        left: 0, 
-        right: 0, 
-        zIndex: 1001, 
-        padding: 0,
+    pathname !== '/sign-up' && (
+      <Paper
+        variant="elavation"
+        elavation={0}
+        sx={{
+          position: 'fixed',
+          bottom: 0,
+          left: 0,
+          right: 0,
+          zIndex: 1001,
+          padding: 0,
         }}
       >
-      <BottomNavigation
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue)
-        }}
-        showLabels
-      >
-        <BottomNavigationAction
-          component={Link}
-          to="/dashboard"
-          label="Home"
-          icon={<HouseIcon />}
-        />
-        <BottomNavigationAction
-          component={Link}
-          to="/browse"
-          label="Browse"
-          icon={<PublicIcon />}
-        />
-        <BottomNavigationAction
-          component={Link}
-          to="/my-courses"
-          label="Courses"
-          icon={<LibraryIcon />}
-        />
-        <BottomNavigationAction
-          component={Link}
-          to="/settings/profile"
-          label="Profile"
-          icon={<AccountIcon />}
-        />
-      </BottomNavigation>
-    </Paper>
-  );
+        <BottomNavigation
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue)
+          }}
+          showLabels
+        >
+          <BottomNavigationAction
+            component={Link}
+            to="/dashboard"
+            label="Home"
+            icon={<HouseIcon />}
+          />
+          <BottomNavigationAction
+            component={Link}
+            to="/browse"
+            label="Browse"
+            icon={<PublicIcon />}
+          />
+          <BottomNavigationAction
+            component={Link}
+            to="/my-courses"
+            label="Courses"
+            icon={<LibraryIcon />}
+          />
+          <BottomNavigationAction
+            component={Link}
+            to="/settings/profile"
+            label="Profile"
+            icon={<AccountIcon />}
+          />
+        </BottomNavigation>
+      </Paper>
+    )
+  )
 }
