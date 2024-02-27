55bb6311178c3b2581332e838f5594bf0e890ffc
AveryDodd <averyhvdodd@gmail.com>
2024-02-27 11:22:36 -0800

    Added Functionality

--- src/components/Dashboard/DashboardVideo.js
+++ src/components/Dashboard/DashboardVideo.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Vimeo from '@u-wave/react-vimeo'
 import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
@@ -6,6 +6,7 @@ import Stack from '@mui/material/Stack'
 import IconButton from '@mui/material/IconButton'
 import EastIcon from '@mui/icons-material/East'
 import useClasses from '../../hooks/useClasses'
+import { useTranslation } from 'react-i18next'
 
 const styles = (theme) => ({
   video: { iframe: { borderRadius: '10px' } },
@@ -13,18 +14,25 @@ const styles = (theme) => ({
 
 function DashboardVideo({ course }) {
   const classes = useClasses(styles)
-
+  const {t} = useTranslation();
+  
   return (
-    <Box sx={{ padding: '30px 0' }}>
-      <Vimeo
-        video={course.videoLinks.split(', ')[0]}
-        responsive
-        width="100%"
-        height="100%"
-        controls={false}
-        // autoplay
-        className={classes.video}
-      />
+    <Box sx={{ padding: '30px 0'}}>
+      <Typography variant="sectionTitle" pt="0px" pb="20px">
+        {t('featured-course')}
+      </Typography>
+      <Box as="div" >
+        <Vimeo
+          video={course.videoLinks.split(', ')[0]}
+          responsive
+          width="100%"
+          height="100%"
+          controls={true}
+          // autoplay
+          className={classes.video}
+        />
+      </Box>
+      
       <Stack
         direction="row"
         spacing={1}
@@ -37,7 +45,7 @@ function DashboardVideo({ course }) {
           alignItems: 'center',
         }}
       >
-        <Typography variant="bold"> {course.seriesName}</Typography>
+        <Typography variant="bold"> {course.seriesName} {t("by")} {course.creator} </Typography>
         <IconButton sx={{ color: 'white' }} href={`/tutorial/${course.uid}`}>
           <EastIcon />
         </IconButton>
